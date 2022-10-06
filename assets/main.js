847583e287ce46847d4b7a3e6f31ea4bf2dc7497
AlexAndHisScripts <alexcj2802@gmail.com>
2022-10-06 16:16:46 +0100

    Changes

--- assets/main.js
+++ assets/main.js
@@ -13,13 +13,32 @@ function pullAndRender() {
             window.lastUpdated = data.lastUpdated;
             window.refreshTime = data.refreshTime;
 
-            data.total.forEach((item, index) => {
+            totalSorted = data.total.sort((a, b) => {
+                return a.local_score > b.local_score
+            })
+
+            document.getElementById("total-table-insert").innerHTML = ""
+
+            totalSorted.forEach((item, index) => {
                 row = document.createElement("tr");
                 positionEl = document.createElement("td");
                 positionEl.innerText = index + 1;
 
-                // TODO continue this. Remember, points are easy,
-                // it's the total leaderboard
+                pointsEl = document.createElement("td")
+                pointsEl.innerText = item.local_score
+
+                nameEl = document.createElement("td")
+                nameEl.innerText = item.name
+
+                starsEl = document.createElement("td")
+                starsEl.innerText = item.stars
+
+                row.appendChild(positionEl)
+                row.appendChild(pointsEl)
+                row.appendChild(nameEl)
+                row.appendChild(starsEl)
+
+                document.getElementById("total-table-insert").appendChild(row)
             })
         })
 }
