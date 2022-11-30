7e4b39d02163375f79f377056ed6b2276607ff49
AlexAndHisScripts <alexcj2802@gmail.com>
2022-11-30 12:11:47 +0000

    Tuning improvements

--- tuner.py
+++ tuner.py
@@ -3,14 +3,14 @@ import math
 
 
 def score(star_time_hours):
-    if star_time_hours < 9.081:
-        return ((-(1 / 5) * star_time_hours) + 35) * 10
+    if star_time_hours < 9.081 * 2:
+        return ((-(1 / 2) * star_time_hours) + 35) * 10
 
-    return (100 / math.sqrt(star_time_hours)) * 10
+    return (100 / math.sqrt(star_time_hours)) * 11.005
 
 
 def plot_score():
-    xAxis = [x / 60 for x in range(0, 48 * 60)]
+    xAxis = [x / 60 for x in range(0, 96 * 60)]
     yAxis = [score(x) for x in xAxis]
     plt.plot(xAxis, yAxis)
 
